b0cbdee1428549535008a184f7bef53978e7a993
Breno Gomes Da Silva <bgsilva3@stefanini.com>
2017-11-24 08:53:41 -0300

    Menu feito.

--- jdm/src/app/app.component.ts
+++ jdm/src/app/app.component.ts
@@ -1,22 +1,61 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
-import { LoginPage } from "../pages/login/login";
+
+
+import { AlunosListaPage } from '../pages/alunos-lista/alunos-lista';
+import { ProfessoresListaPage } from '../pages/professores-lista/professores-lista';
+import { EventosListaPage } from '../pages/eventos-lista/eventos-lista';
+import { NoticiasListaPage } from '../pages/noticias-lista/noticias-lista';
+import { ImagensListaPage } from '../pages/imagens-lista/imagens-lista';
+import { SobrePage } from '../pages/sobre/sobre';
+import { LocalizacaoPage } from '../pages/localizacao/localizacao';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
 })
+
 export class MyApp {
-  rootPage:any = LoginPage;
+  
+  @ViewChild('NAV') nav: Nav;
+  public rootPage:any;
+  public pages: Array<{title: string, component: any, icon: string}>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  
 
-      statusBar.styleDefault();
-      splashScreen.hide();
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  
+    this.rootPage = LoginPage;
+  this.pages = [
+      
+      { title: 'Home', component: HomePage, icon: 'home' },
+
+      { title: 'Sobre', component: SobrePage, icon: 'school'},
+
+      { title: 'Alunos', component: AlunosListaPage, icon: 'people'},
+
+      { title: 'Professores', component: ProfessoresListaPage, icon: 'people'},  
+
+      { title: 'Eventos', component: EventosListaPage, icon: 'calendar' },
+
+      { title: 'Imagens', component: ImagensListaPage, icon: 'images'},
+
+      { title: 'Noticias', component: NoticiasListaPage, icon: 'images'},
+
+      { title: 'Localizacao', component: LocalizacaoPage, icon: 'navigate'}
+      
+    ];
+    
+      this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
-}
 
+  goToPage(page) {
+    this.nav.setRoot(page);
+  }
+}
